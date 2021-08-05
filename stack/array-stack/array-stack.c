c71eeac047ef297c145395b16d282f458c1eeb84
Matheus Feres Turcheti <matheusft.mft@gmail.com>
2021-08-05 12:51:58 -0300

    Create array-stack

---
+++ stack/array-stack/array-stack.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "array-stack.h"
+
+stack* salloc(int capacity){
+    stack* s = malloc(sizeof(stack));
+    if (s) {
+        s->data = (int*) malloc(capacity * sizeof(int));
+        s->capacity = capacity;
+        s->last = -1;
+        s->size = 0;
+    }
+    return s;
+}
+
+int resize_stack(stack* s, float factor){
+    int* array = malloc(factor * s->capacity * sizeof(int));
+    if (array == NULL) 
+        return 0;
+    
+    s->capacity *= factor;
+    for (int i = 0; i < s->size; ++i)
+        array[i] = s->data[i];
+
+    free(s->data);
+    s->data = (int*) malloc(sizeof(int));
+    s->data = array;
+    free(array);
+    
+    return 1;
+}
+
+int s_push(stack* s, int key){
+    if (s == NULL) return 0;
+
+    if (s->size == s->capacity)
+        if ( !resize_stack(s, 2) ) return 0;  // this will try to resize and continue accordingly
+
+    s->data[++s->last] = key;  // this works because the increment is done *before* the access
+    s->size++;
+    return 1;
+}
+
+int* s_pop(stack* s){
+    if (s == NULL) return s;
+    else if (s->last >= 0) {
+        if (s->size <= 0.25 * s->capacity)
+            resize_stack(s, 0.5);
+        --s->size;
+        int* popped = &s->data[s->last--];  // this works because the increment is done *after* the access
+        return popped;
+    }
+    else return NULL;
+}
+
+void free_stack(stack* s){
+    if (s) {
+        // if q->data stores pointers of any kind, they should be freed too
+        free(s->data);
+    } free(s);
+}
+
+void print_s(stack* s){
+    if (s == NULL)
+        printf("There's no stack.\n");
+    else if (s->size == 0)
+        printf("The stack is empty.\n");
+    else {
+        for (int i = s->last; i > 0; i--)
+            printf("| %d ", s->data[i]);
+        printf("| %d |\n", s->data[0]);
+    }
+}
---
+++ stack/array-stack/array-stack.h
@@ -0,0 +1,56 @@
+/// Stack structure
+typedef struct array_stack {
+    int* data;  // dynamic array of ints
+    int capacity;  // current maximum amount of data points permitted (ideally an even number)
+    int last;  // index of the stack's top element 
+    int size;  // amount of data points stored
+} stack;
+
+/**
+ * @brief Allocates memory and starts an empty stack.
+ * 
+ * @param capacity initial number of data points supported
+ * @return the address of the stack created or NULL, if it failed. 
+ */
+stack* salloc(int capacity);
+
+/**
+ * @brief Changes the array supporting the stack by multipying its capacity by a given factor.
+ * 
+ * @param s stack being resized
+ * @param factor ratio between the new capacity and the old one
+ * @return 1 on success. 0 on failure.
+ */
+int resize_stack(stack* s, float factor);
+
+/**
+ * @brief Appends a data point in the array. 
+ * If the darray is full, it tries to double its capacity and then adds the key.
+ * When resizing the array is not possible, it will not push. 
+ * 
+ * @param s stack in which data is added
+ * @param key the data inserted
+ * @return 1 on success. 0 on failure.
+ */
+int s_push(stack* s, int key);
+
+/**
+ * @brief Removes, by invalidating its position with the `last` field, a data point from the stack.
+ * If the darray is a quarter full, it tries to halve its capacity, no matter how small the array already is. 
+ * When resizing the array is not possible, it will pop without changing the capacity.
+ * 
+ * @return pointer to the data point invalidated. NULL if the stack is empty or null. 
+ */
+int* s_pop(stack* s);
+
+/**
+ * @brief Releases a whole stack deallocating memory.
+ */
+void free_stack(stack* s);
+
+/**
+ * @brief Displays all the contents in a stack (LIFO, of course) separeted by '|'.
+ * 
+ * @param s stack being printed.
+ */
+void print_s(stack* s);
